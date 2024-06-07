2dfed77623ee1f64116b37d3f7d05734b17f38c4
Sujit <aaressinfomedia@gmail.com>
2024-06-07 09:52:41 +0530

    CSS Updated & Error Page Corrected

--- src/pages/E404.jsx
+++ src/pages/E404.jsx
@@ -3,7 +3,7 @@ import errorImg from "../assets/ErrorPage.gif";
 
 const E404 = () => {
   return (
-    <div className="contianer-fluid d-block p-5 h-50">
+    <div className="contianer-fluid d-block p-5 mt-5">
       <h2>Error Page</h2>
       <img
         src={errorImg}
